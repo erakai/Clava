3bb181f6f6763fae51cd9c7add19245685515e7c
Chrislee3x7 <32285657+Chrislee3x7@users.noreply.github.com>
2023-02-19 18:50:38 -0500

    finished barebones ui for login page

--- client/src/views/Login/Login.tsx
+++ client/src/views/Login/Login.tsx
@@ -1,11 +1,35 @@
+import { Box, TextField, Stack, Typography, Button, IconButton } from "@mui/material"
+import {ArrowBack} from "@mui/icons-material"
 import { getUsers } from "api/user"
+import { grey } from "@mui/material/colors"
 
 function Login() {
   return (
-    <div className="Login">
-      <h1>Hi</h1>
-      <p>This is the login page</p>
-    </div>
+    <Box className="Login flex w-screen h-screen items-center justify-center">
+      <Stack 
+        spacing={2} 
+        className="bg-emerald-300 items-center m-8 p-8"
+        sx={{ borderRadius: '2%', borderColor: 'grey.500' }}
+        >
+        <Stack className="flex-row w-full items-center" direction="row">
+          <IconButton>
+            <ArrowBack />
+          </IconButton>
+          <Typography variant="h5" component="h1">Login</Typography>
+        </Stack>
+        <TextField
+          id="email-text-field"
+          label="Email"
+          variant="outlined"
+          type="email"/>
+        <TextField
+          id="password-text-field"
+          label="Password"
+          variant="outlined"
+          type="password"/>
+        <Button variant="contained">Login</Button>
+      </Stack>
+    </Box>
   )
 }
   
