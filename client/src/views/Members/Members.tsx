7875d26e6c82dc29e5e6fa7bd3fad649f7aecd72
AlexHunton2 <alexhunton02@gmail.com>
2023-02-19 21:43:27 -0500

    Initial Branch Commit

---
+++ client/src/views/Members/Members.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+function Members() {
+  return (
+    <div className="Members">
+      <h1>Members</h1>
+    </div>
+  )
+}
+
+export default Members
