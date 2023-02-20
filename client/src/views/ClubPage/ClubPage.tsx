feb708e1071f9840d5ef5767dc828fa1049556d9
Leo <kinglionleo.8@gmail.com>
2023-02-19 23:24:33 -0500

    Added some cards

--- client/src/views/ClubPage/ClubPage.tsx
+++ client/src/views/ClubPage/ClubPage.tsx
@@ -1,5 +1,5 @@
 import { getUsers } from "api/user"
-import { Box, Stack, Paper } from '@mui/material'
+import { Box, Stack, Paper, Card, CardContent, CardMedia, CardActionArea, CardActions, Typography, Button } from '@mui/material'
 
 function ClubPage() {
   return (
@@ -13,9 +13,79 @@ function ClubPage() {
       <h1>Clubs</h1>
       <p>These are the clubs you are in</p>
 
-      <Paper>Club 1</Paper>
-      <Paper>Club 2</Paper>
-      <Paper>Club 3</Paper>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            src="placeholder.png"
+            alt="image failed load"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Book Club
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Description for Book Club
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Edit
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            src="placeholder.png"
+            alt="image failed load"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Book Club
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Description for Book Club
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Edit
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            src="placeholder.png"
+            alt="image failed load"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Book Club
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Description for Book Club
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Edit
+          </Button>
+        </CardActions>
+      </Card>
+
+      
 
       </Stack>
     </Box>
