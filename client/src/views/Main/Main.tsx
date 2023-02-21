31b4c4433965c27b92c67232603c741d83a2270d
Chrislee3x7 <32285657+Chrislee3x7@users.noreply.github.com>
2023-02-20 23:12:57 -0500

    revamped main page temporarily

--- client/src/views/Main/Main.tsx
+++ client/src/views/Main/Main.tsx
@@ -1,13 +1,23 @@
 import { getUsers } from "api/user"
+import { Box, Button, Stack } from "@mui/material"
 import { Link } from "react-router-dom"
 
 function Main() {
   return (
-    <div className="Main">
-      <h1>Sup</h1>
-      <Link to="login">Login</Link>
+    <Box className="Main bg-emerald-300 flex w-screen h-screen items-center justify-center" flexDirection="column">
+      <h1>Clava</h1>
+      <Stack
+        className="flex-row" flexDirection="row">
+        <Link to="/login">
+          <Button variant="outlined">Login</Button>
+        </Link>
+        <Box className="flex-1 w-5" />
+        <Link to="/register">
+          <Button variant="contained">Register</Button>
+        </Link>
+      </Stack>
       <p>This will be the main homepage of the site</p>
-    </div>
+    </Box>
   )
 }
   
