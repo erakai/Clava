b6a01532cd570ad376df556a4633918d6d083f4c
Kris Leungwattanakij <kleungwa@purdue.edu>
2023-02-23 02:02:43 -0500

    Changed styling of login page and added sign up button

--- client/src/views/Login/Login.tsx
+++ client/src/views/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { Box, TextField, Stack, Typography, Button, IconButton } from "@mui/material"
+import { Divider, Box, TextField, Stack, Typography, Button, IconButton} from "@mui/material"
 import { Link } from "react-router-dom"
 import {ArrowBack} from "@mui/icons-material"
 import { getUsers } from "api/user"
@@ -8,7 +8,7 @@ function Login() {
     <Box className="Login flex w-screen h-screen items-center justify-center">
       <Stack 
         spacing={2} 
-        className="bg-emerald-300 items-center m-8 p-8 max-w-md"
+        className="bg-emerald-300 items-center m-8 p-8 max-w-md px-10"
         sx={{ borderRadius: '2%' }}>
         <Stack 
           spacing={1}
@@ -20,27 +20,31 @@ function Login() {
           <Typography variant="h5" component="h1">Login</Typography>
         </Stack>
         <TextField
-          className="w-[85%]"
+          className="w-[100%]"
           id="email-text-field"
           label="Email"
           variant="outlined"
           type="email"/>
         <TextField
-          className="w-[85%]"
+          className="w-[100%]"
           id="password-text-field"
           label="Password"
           variant="outlined"
           type="password"/>
         <Box
-          className="flex w-[85%] items-center">
+          className="flex w-[95%] items-center justify-center">
+          <Button variant="contained" className="mx-4 w-[100%]">Login</Button>
+        </Box>
+        <div>
           <Link to="/Reset" className="mx-4">
             <Button variant="text">Forgot password?</Button>
           </Link>
-          <Button variant="contained" className="mx-4">Login</Button>
-        </Box>
-        <Link to="/Register">
-          <Button variant="text">Don't have a Clava account yet?</Button>
-        </Link>
+          <Divider className="pb-5 pt-3">OR</Divider>
+          <Box
+              className="flex w-[95%] items-center justify-center">
+            <Button component={Link} to="/Register" variant="contained" className="mx-4 w-[100%]">Sign Up</Button>
+          </Box>
+        </div>
       </Stack>
     </Box>
   )
