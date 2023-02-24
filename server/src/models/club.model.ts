58bba55e827591a2a30420875d3550d2fea62893
Leo <kinglionleo.8@gmail.com>
2023-02-24 09:26:36 -0500

    Added club model

---
+++ server/src/models/club.model.ts
@@ -0,0 +1,22 @@
+import { Model, model, Schema, SchemaTypes } from "mongoose";
+import { IClub } from "types/club";
+
+const ClubSchema = new Schema<IClub>({
+  club_id: SchemaTypes.ObjectId,
+  name: {
+    type: SchemaTypes.String, 
+    required: true,
+  },
+  description: SchemaTypes.String,
+  tag_ids: [SchemaTypes.ObjectId],
+  role_ids: [SchemaTypes.ObjectId],
+  tran_ids: [SchemaTypes.ObjectId],
+  reim_ids: [SchemaTypes.ObjectId],
+  member_ids: [SchemaTypes.ObjectId],
+  officer_ids: [SchemaTypes.ObjectId],
+  event_ids: [SchemaTypes.ObjectId]
+})
+
+const Club = model('club', ClubSchema) as Model<IClub>
+
+export default Club
