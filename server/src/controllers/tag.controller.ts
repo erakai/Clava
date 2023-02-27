42ca07b504d38b34722128c407f2d1076e25b036
Chrislee3x7 <32285657+Chrislee3x7@users.noreply.github.com>
2023-02-27 13:57:19 -0500

    finished basic creation and get functionality backend for tags

--- server/src/controllers/tag.controller.ts
+++ server/src/controllers/tag.controller.ts
@@ -19,71 +19,49 @@ export const getTags = async (req: Request, res: Response) => {
 
   const tags = []
 
-  // get all tags from tag_ids and put all tags into tag array 
-  for (let i = 0; i < tag_ids.length; i++) {
-    Tag.find({
-      tag_id: tag_ids[i]
-    }, async (err, tag) => {
-      if (err) {
-        return res.status(500).send({err})
-      }
-      // add tag to array of tags if was able to be found
-      tags.push(tag)
-    })
-  }
+  Tag.find({
+    club_id: club_id
+  }, async (err, tags) => {
+    if (err) {
+      return res.status(500).send({err})
+    }
 
-  return res.status(200).json({tags})
+    return res.status(200).json({tags})
+  })
 }
 
 export const createTag = async (req: Request, res: Response) => {
-  let { club_id, tagName, tagColor, } = req.body
+  let { club_id, tag_name, tag_color, } = req.body
 
   if (!club_id) {
     return res.status(500).json({error: 'no club id provided'})
   }
 
-  if (!tagName || !tagColor) {
+  if (!tag_name || !tag_color) {
     return res.status(500).json({error: 'no tag name or tag color provided'})
   }
 
+  // verify that the club exists
+
+  Club.find({
+    club_id: club_id
+  }, async (err) => {
+    if (err) {
+      return res.status(500).send({err})
+    }
+  })
+
   // TODO: verify that tag is unique
 
+  // add the new tag to the club
   Tag.create({
-    tagName, tagColor
+    tag_name, tag_color, club_id
   }, async (err, tag) => {
     if (err) {
       return res.status(500).send({err})
     }
     return res.status(200).json({tag})
   })
-}
 
-export const isUniqueTagName = async (req: Request, res: Response) => {
-  let { tagName, club_id } = req.body
-  if (!club_id || !tagName) {
-    return res.status(500).json({error: 'no club id or tag name provided'})
-  }
-
-  const club: IClub = await Club.findById(club_id)
-
-  if (!club) {
-    return res.status(401).send('Unauthorized')
-  }
-
-  const tag_ids = club.tag_ids
-
-  const tags = []
-
-  // get all tags from tag_ids and put all tags into tag array 
-  for (let i = 0; i < tag_ids.length; i++) {
-    Tag.find({
-      tag_id: tag_ids[i]
-    }, async (err, tag) => {
-      if (err) {
-        return res.status(500).send({err})
-      }
-      // add tag to array of tags if was able to be found
-      tags.push(tag)
-    })
-  }
+  
 }
--- server/src/types/tag.d.ts
+++ server/src/types/tag.d.ts
@@ -3,5 +3,6 @@ import { Document } from "mongoose"
 interface ITag extends Document {
   tag_id: number,
   name: string,
+  club_id: number,
   color: string
 }
