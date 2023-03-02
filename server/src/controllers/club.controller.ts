779f6136f8a3a635baf54031730693604f267845
Leo <kinglionleo.8@gmail.com>
2023-03-02 16:24:32 -0500

    Club backend

--- server/src/controllers/club.controller.ts
+++ server/src/controllers/club.controller.ts
@@ -6,35 +6,50 @@ import { IUser } from 'types/user'
 import { IClub } from 'types/club'
 
 export const getClubs = async (req: Request, res: Response) => {
-  let { user_id } = req.body
+  let { user_id } = req.query
+  
   if (!user_id) {
     return res.status(500).json({error: 'no user id'})
   }
-
+  
   const user: IUser = await User.findById(user_id)
-
+  
   if(!user) return res.status(401).send('Unauthorized')
-
+  
   const club_ids = user.club_ids;
-
+  
   const clubs = []
-
+  
   for(let i=0; i<club_ids.length; i++) {
     
     const club: IClub = await Club.findById(club_ids[i])
-
+    
     if(!club) return res.status(401).send('Unauthorized')
-
+    
     clubs.push(club)
-
+    
   }
 
   return res.status(200).json({clubs})
 }
 
+export const getClub = async (req: Request, res: Response) => {
+  let { club_id } = req.query
+  
+  if (!club_id) {
+    return res.status(500).json({error: 'no club id'})
+  }
+    
+  const club: IClub = await Club.findById(club_id)
+  
+  if(!club) return res.status(401).send('Unauthorized')
+    
+  return res.status(200).json({club})
+}
+
 export const createClub = async (req: Request, res: Response) => {
   let { name, description } = req.body
- 
+  
 
   if (!name) {
     return res.status(500).json({error: 'no name provided'})
@@ -52,5 +67,55 @@ export const createClub = async (req: Request, res: Response) => {
     }
 
     return res.status(200).json({club})
+  })
+}
+
+export const addClubToUser = async (req: Request, res: Response) => {
+  let { user_id, club_id } = req.body
+  
+  console.log("im back")
+
+  if (!user_id) {
+    return res.status(500).json({error: 'no user id provided'})
+  }
+
+  if (!club_id) {
+    return res.status(500).json({error: 'no club id provided'})
+  }
+
+  User.findByIdAndUpdate(user_id, { $push: {"club_ids": club_id} }, async (err, result) => {
+
+    if(err){
+      return res.status(500).send({err})
+    }
+    else{
+      return res.status(200).json({result})
+    }
+
+  })
+}
+
+export const removeClubFromUser = async (req: Request, res: Response) => {
+  let { user_id, club_id } = req.body
+  
+  console.log("im back")
+
+  if (!user_id) {
+    return res.status(500).json({error: 'no user id provided'})
+  }
+
+  if (!club_id) {
+    return res.status(500).json({error: 'no club id provided'})
+  }
+
+  User.findByIdAndUpdate(user_id, { $pull: {"club_ids": club_id} }, async (err, result) => {
+
+    if(err){
+      return res.status(500).send({err})
+    }
+    else{
+      return res.status(200).json({result})
+    }
+
   })
 }
