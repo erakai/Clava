5e51b2cf11b8d243c4b06159b64bb46a48595d9d
Kris Leungwattanakij <kleungwa@purdue.edu>
2023-03-21 22:28:45 -0400

    Restructured EventView and created "create event" button

--- client/src/views/Events/EventView.tsx
+++ client/src/views/Events/EventView.tsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react"
+import {Box, Button, Grid} from "@mui/material";
 
 export default function EventView() {
+  const [createEventOpen, setCreateEventOpen] = useState(false);
+
   return (
-    <div className="p-2 items-center">
-      <h1>Events</h1>
-    </div>
+    <Box className="m-4 mb-16">
+      <Grid container spacing={1}>
+        <Grid item xs={12} md={6} lg={3}>
+          <Box className="min-w-full flex-auto">
+            <Button className='h-full' variant="contained" color="secondary" onClick={() => setCreateEventOpen(true)}>
+              Create Event
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
